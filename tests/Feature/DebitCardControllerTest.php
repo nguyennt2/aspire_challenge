c6477e9e590a3aa1f52cbafa9a29a5f6419b7ced
Nguyen Nguyen Tuan <nguyennguyentuan@Nguyens-MacBook-Air.local>
2021-03-18 16:09:34 +0700

    feat: apply unit debit card

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\DebitCard;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -12,42 +13,95 @@ class DebitCardControllerTest extends TestCase
     use RefreshDatabase;
 
     protected User $user;
+    protected DebitCard $debitCard;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->user = User::factory()->create();
+        $this->debitCard = DebitCard::factory()->create();
         Passport::actingAs($this->user);
     }
 
     public function testCustomerCanSeeAListOfDebitCards()
     {
         // get /debit-cards
+        $response = $this->get('/debit-cards');
+        $response->assertStatus(200);
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has('id')
+                ->has('number')
+                ->has('type')
+                ->has('expiration_date')
+                ->has('is_active')
+        );
     }
 
     public function testCustomerCannotSeeAListOfDebitCardsOfOtherCustomers()
     {
         // get /debit-cards
+        $response = $this->get('/debit-cards');
+        $response->assertStatus(200);
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->missing('id')
+                ->missing('number')
+                ->missing('type')
+                ->missing('expiration_date')
+                ->missing('is_active')
+        );
     }
 
     public function testCustomerCanCreateADebitCard()
     {
         // post /debit-cards
+        $response = $this->post('/debit-cards', ['type' => 'DEBIT']);
+
+        $response->assertStatus(201);
     }
 
     public function testCustomerCanSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+        $response = $this->get('/debit-cards/', ['debitCard' => $this->debitCard]);
+
+        $card = $this->debitCard->get($this->debitCard);
+
+        $response->assertStatus(200);
+
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('id', $card->id)
+                ->where('number', $card->number)
+                ->where('type', $card->type)
+                ->where('expiration_date', $card->expiration_date)
+                ->where('is_active', $card->is_active)
+                ->etc()
+
+        );
     }
 
     public function testCustomerCannotSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+        $response = $this->get('/debit-cards/', ['debitCard' => $this->debitCard]);
+
+        $response->assertStatus(200);
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->missing('id')
+                ->missing('number')
+                ->missing('type')
+                ->missing('expiration_date')
+                ->missing('is_active')
+                ->etc()
+
+        );
     }
 
     public function testCustomerCanActivateADebitCard()
     {
         // put api/debit-cards/{debitCard}
+        $response = $this->put('/debit-cards/', ['debitCard' => $this->debitCard]);
+
+        $response->assertStatus(202);
     }
 
     public function testCustomerCanDeactivateADebitCard()
